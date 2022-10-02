8313d5a921c1b639f7c09178d328add81751dfc0
mehmet <sayinmehmet47@gmail.com>
2022-10-02 14:36:21 +0200

    feat: new question add

---
+++ SoloLearn/script.js
@@ -0,0 +1,21 @@
+'🚀 ';
+// var set = new Set([1, 2, 0, 2, 4]);
+// var map = new Map([set], [set]);
+// console.log('map', map.size);  //map:1
+// console.log('set' + set.size);  //set:4
+
+'🚀 ';
+
+// (() => console.log(typeof arguments))(); Object
+
+'🚀 ';
+// function foo(a, b, c, d) {
+//   return a * b + c - d;
+// }
+
+// var x = foo(2, 4, 2, 6);
+
+// for (i = 0; i < 10; i++) {
+//   x++;
+// }
+// console.log(x);
